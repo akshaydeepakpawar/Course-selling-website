9bd6cc65ca18de232cb58b4b961acea7b775e291
akshay pawar <ap3690700@gmail.com>
2025-09-02 13:11:03 +0530

    router is added

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose =require("mongoose")
 const Schema= mongoose.Schema;
 const ObjectId=Schema.Types.ObjectId;
 
-mongoose.connect("");
+mongoose.connect(""); 
 
 const userSchema =new Schema({
     email:{type:String, unique:true},
